c207dbe713fb07912937405a2daab55836061d03
postblue <postblue@postblue.info>
2017-01-23 13:27:59 +0100

    It's a function

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,9 @@
+jQuery(function($) {
 /* Invoke ScrollUpBar Plugin
    FIXME: Does it even work?
    1. TypeError: $(...).scrollupbar is not a function
    2. When loaded, topbar background won't change it color
 */
 // $('#topbar').scrollupbar();
+
+});
